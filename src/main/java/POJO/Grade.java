be10a23208c60ff02d4f5dbd1f7d8336d825c704
Evrusx2 <wojciech.drywa@outlook.com>
2020-03-24 10:02:21 +0100

    Added new POJO classes

---
+++ src/main/java/POJO/Grade.java
@@ -0,0 +1,32 @@
+package POJO;
+
+public class Grade {
+
+    private int grade;
+    private String gradeName;
+    private int gradeWage;
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public String getGradeName() {
+        return gradeName;
+    }
+
+    public void setGradeName(String gradeName) {
+        this.gradeName = gradeName;
+    }
+
+    public int getGradeWage() {
+        return gradeWage;
+    }
+
+    public void setGradeWage(int gradeWage) {
+        this.gradeWage = gradeWage;
+    }
+}
---
+++ src/main/java/POJO/Subject.java
@@ -0,0 +1,23 @@
+package POJO;
+
+public class Subject {
+
+    private String subjectName;
+    private int subjectID;
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getSubjectID() {
+        return subjectID;
+    }
+
+    public void setSubjectID(int subjectID) {
+        this.subjectID = subjectID;
+    }
+}
